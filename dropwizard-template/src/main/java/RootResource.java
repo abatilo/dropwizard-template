5118a6ee564d4bcbd8d181f6ae5c895faa2d4fdb
Aaron <AaronBatilo@gmail.com>
2019-07-15 07:59:20 -0600

    Add structured json logging

--- dropwizard-template/src/main/java/RootResource.java
+++ dropwizard-template/src/main/java/RootResource.java
@@ -2,12 +2,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Path("/")
 @RequiredArgsConstructor
 public class RootResource {
   @GET
   public String root() {
+    log.info("Hello World, structured logging test");
     return "Hello World";
   }
 }
